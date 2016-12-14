0eea13e7d6a553eacc36081978e579aae2d39990
David Arnold <dar@devco.co>
2016-12-13 23:13:09 -0500

    [FIX] partner and company adress behaviour
    - refactoring of onchanges to perfection.
    - clean up and reduction of lines of code
    - somewhat duplicated code in partner and company, this is how partner and company are percieved in base code... thwo methods are inherently class dependent (super calls), so the benefit is limited and we would have concept micxing
    
    Signed-off-by: David Arnold <dar@devco.co>
    
    Signed-off-by: David Arnold <dar@devco.co>

--- l10n_co_geo/models/res_partner.py
+++ l10n_co_geo/models/res_partner.py
@@ -8,22 +8,55 @@ from openerp import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    city = fields.Char(
-        invisible=True
-    )
+    city_id = fields.Many2one('res.country.state.city', 'City')
 
-    state_id = fields.Many2one(
-        'res.country.state',
-        related='city_id.state_id',
-        readonly=True,
-    )
-
-    city_id = fields.Many2one(
-        comodel_name='res.country.state.city',
-        string=u'City',
-        ondelete='cascade'
-    )
+    def _enforce_address_domains(self):
+        domain = dict(city_id=[], state_id=[])
+        if self.country_id:
+            domain['state_id'] = [('country_id', '=', self.country_id.id)]
+            domain['city_id'] = [('country_id', '=', self.country_id.id)]
+        if self.state_id:
+            domain['city_id'] = [('state_id', '=', self.state_id.id)]
+        return {'domain': domain}
 
     @api.onchange('city_id')
-    def onchange_city(self):
+    def _onchange_city(self):
         self.city = self.city_id.name
+        if self.city_id:
+            self.state_id = self.city_id.state_id
+            self.country_id = self.city_id.country_id
+        return self._enforce_address_domains()
+
+    @api.onchange('state_id')
+    def _onchange_state(self):
+        if self.state_id:
+            if self.city_id.state_id != self.state_id:
+                self.city_id = False
+            self.country_id = self.state_id.country_id
+        return self._enforce_address_domains()
+
+    @api.onchange('country_id')
+    def _onchange_country(self):
+        if self.country_id:
+            if self.city_id.country_id != self.country_id:
+                self.city_id = False
+            if self.state_id.country_id != self.country_id:
+                self.state_id = False
+        return self._enforce_address_domains()
+
+    @api.model
+    def _address_fields(self):
+        return super(ResPartner, self)._address_fields() + ['city_id']
+
+    @api.model
+    def create(self, vals):
+        if vals.get('city_id'):
+            vals['state_id'] = self.env[
+                'res.country.state.city'
+            ].browse(vals.get('city_id')).state_id.id
+        if vals.get('state_id'):
+            vals['country_id'] = self.env[
+                'res.country.state'
+            ].browse(vals.get('state_id')).country_id.id
+
+        return super(ResPartner, self).create(vals)
