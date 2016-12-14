1c58a63f63271e2a1afc012cf673e092fd6096fe
David Arnold <dar@devco.co>
2016-12-13 23:16:19 -0500

    [FIX] fix States and Cities creation, fix city form
    - it is now really easi to create new cities and states
    - cities can be created without states, this is taken into account in the domains of the relevant address fields, so that cities without a state can be selected, if a state is selected. Some countries do not use states very much.
    - sane defaults upon creation
    - Line of code reduction
    
    Signed-off-by: David Arnold <dar@devco.co>

--- l10n_co_geo/models/__init__.py
+++ l10n_co_geo/models/__init__.py
@@ -3,8 +3,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import (
-    res_country_state_city,
-    res_country_state,
     res_country,
     res_partner,
     res_company
--- l10n_co_geo/models/res_country.py
+++ l10n_co_geo/models/res_country.py
@@ -2,19 +2,56 @@
 # Copyright 2016 David Arnold, DevCO Colombia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class ResCountry(models.Model):
     _inherit = 'res.country'
 
-    code_dian = fields.Char(
-        string='Code Dian',
-        required=False,
-        readonly=False,
-        index=False,
-        default=None,
-        help=False,
-        size=3,
-        translate=False
-    )
+    code_dian = fields.Char('Code Dian', size=3, translate=False)
+
+
+class ResState(models.Model):
+    _inherit = 'res.country.state'
+
+    iso = fields.Char('iso', size=3, translate=False)
+
+
+class ResCity(models.Model):
+    _name = 'res.country.state.city'
+    _description = 'City'
+
+    name = fields.Char('City', required=True)
+    state_id = fields.Many2one('res.country.state', 'State')
+    statcode = fields.Char(
+        'DANE Code', size=5, translate=False,
+        help='Code of the Colombian statistical department')
+    country_id = fields.Many2one('res.country', 'Country', required=True)
+
+    def _enforce_address_domains(self):
+        domain = dict(state_id=[])
+        if self.country_id:
+            domain['state_id'] = [('country_id', '=', self.country_id.id)]
+        return {'domain': domain}
+
+    @api.onchange('state_id')
+    def _onchange_state(self):
+        if self.state_id:
+            self.country_id = self.state_id.country_id
+        return self._enforce_address_domains()
+
+    @api.onchange('country_id')
+    def _onchange_country(self):
+        if self.country_id:
+            if self.state_id.country_id != self.country_id:
+                self.state_id = False
+        return self._enforce_address_domains()
+
+    @api.model
+    def create(self, vals):
+        if vals.get('state_id'):
+            vals['country_id'] = self.env[
+                'res.country.state'
+            ].browse(vals.get('state_id')).country_id.id
+
+        return super(ResCity, self).create(vals)
